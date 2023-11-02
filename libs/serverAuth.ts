9239b355397bdf43dbf0c38ab322395709edb46b
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-11-02 11:36:37 -0300

    Several things

---
+++ libs/serverAuth.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest } from "next";
+import { getSession } from "next-auth/react";
+
+import prisma from "@/libs/prismadb";
+
+const serverAuth = async (req: NextApiRequest) => {
+    const session = await getSession({ req });
+
+    if (!session?.user?.email) {
+        throw new Error('Not signed in');
+    }
+
+    const currentUser = await prisma.user.findUnique({
+        where: { email: session.user.email }
+    });
+
+    if (!currentUser) {
+        throw new Error('User not found');
+    }
+
+    return { currentUser };
+};
+
+export default serverAuth;
