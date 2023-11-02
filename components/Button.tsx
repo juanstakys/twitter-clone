0a1369275e3938003563f268009240b1cce14ce4
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-11-01 21:52:57 -0300

    Button corrections

--- components/Button.tsx
+++ components/Button.tsx
@@ -29,16 +29,16 @@ const Button: React.FC<ButtonProps> = ({
             hover-opacity-70
             transition
             border-2
-            $(fullWidth ? "w-full" : "")
-            $(secondary ? "bg-white : "bg-sky-500")
-            $(secondary ? "text-black" : "text-white")
-            $(secondary ? "boder-black" : "border-sky-500")
-            $(large ? "text-xl" : "text-md")
-            $(large ? "px-5" : "px-4")
-            $(large ? "py-3" : "py-2")
-            $(outline ? "bg-transparent" : "")
-            $(outline ? "border-white" : "")
-            $(outline ? "text-white" : "")
+            ${fullWidth ? "w-full" : ""}
+            ${secondary ? "bg-white" : "bg-sky-500"}
+            ${secondary ? "text-black" : "text-white"}
+            ${secondary ? "boder-black" : "border-sky-500"}
+            ${large ? "text-xl" : "text-md"}
+            ${large ? "px-5" : "px-4"}
+            ${large ? "py-3" : "py-2"}
+            ${outline ? "bg-transparent" : ""}
+            ${outline ? "border-white" : ""}
+            ${outline ? "text-white" : ""}
             `}
         >
             {label}</button>
