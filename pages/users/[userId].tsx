c89ba595e575f6224809ac7c089da5669a1f83da
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-11-06 16:55:27 -0300

    UserHero & Spinners

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,7 +1,27 @@
+import Header from "@/components/Header"
+import UserHero from "@/components/users/UserHero"
+import useUser from "@/hooks/useUser"
+import { useRouter } from "next/router"
+import { ClipLoader } from "react-spinners"
+
 const UserView = () => {
+    const router = useRouter()
+    const { userId } = router.query
+
+    const { data: fetchedUser, isLoading } = useUser(userId as string)
+
+    if (isLoading || !fetchedUser) {
+        return (
+            <div className="flex justify-center items-center h-full">
+                <ClipLoader color="lightblue" size={80} />
+            </div>
+        )
+    }
+
     return (
         <div>
-            <h1>User View</h1>
+            <Header showBackArrow label={fetchedUser?.name} />
+            <UserHero userId={userId as string} />
         </div>
     )
 }
