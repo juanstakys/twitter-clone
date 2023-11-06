aaac2f0349f112a9d642aa6d839abea5a973c065
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-11-06 15:51:52 -0300

    Avatars and followBar

---
+++ components/Avatar.tsx
@@ -0,0 +1,50 @@
+import useUser from "@/hooks/useUser";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+import Image from 'next/image'
+
+interface AvatarProps {
+    userId: string;
+    isLarge?: boolean;
+    hasBorder?: boolean;
+}
+
+const Avatar: React.FC<AvatarProps> = ({
+    userId,
+    isLarge,
+    hasBorder
+}) => {
+    const router = useRouter()
+    const { data: fetchedUser } = useUser(userId)
+
+    const onClick = useCallback((event: any) => {
+        event.stopPropagation()
+
+        const url = '/users/' + userId
+
+        router.push(url)
+    }, [router, userId])
+
+    return (
+        <div className={`
+        ${hasBorder ? 'border-4 border-black' : ''}
+        ${isLarge ? 'w-32 h-32' : 'w-12 h-12'}
+        rounded-full
+        hover-opacity-90
+        transition
+        cursor-pointer
+        relative
+        `}>
+            <Image fill style={{
+                objectFit: 'cover',
+                borderRadius: '100%'
+            }}
+                alt="Avatar"
+                onClick={onClick}
+                src={fetchedUser?.profileImage || '/images/default-avatar.png'}
+            />
+        </div>
+    )
+}
+
+export default Avatar;
---
+++ pages/users/[userId].tsx
@@ -0,0 +1,9 @@
+const UserView = () => {
+    return (
+        <div>
+            <h1>User View</h1>
+        </div>
+    )
+}
+
+export default UserView
