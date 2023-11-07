b111efa9b5c11477999e9dfd02a3dbdfabc37b03
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-11-07 20:39:30 -0300

    Tweet form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import Form from "@/components/Form";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
       <Header label="Home"></Header>
+      <Form placeholder="What's happening?" />
     </>
   )
 }
