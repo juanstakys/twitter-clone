58aba827da2d02f7516924a3e4fca103e644950c
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-11-07 21:56:24 -0300

    PostFeed and PostItem

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header"
+import PostFeed from "@/components/posts/PostFeed"
 import UserBio from "@/components/users/UserBio"
 import UserHero from "@/components/users/UserHero"
 import useUser from "@/hooks/useUser"
@@ -24,6 +25,7 @@ const UserView = () => {
             <Header showBackArrow label={fetchedUser?.name} />
             <UserHero userId={userId as string} />
             <UserBio userId={userId as string} />
+            <PostFeed userId={userId as string} />
         </div>
     )
 }
